794bae7d66692c64b86e2523ae91cd5b423c5a57
Edward Tanguay <edward@tanguay.info>
2021-06-03 22:19:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// Answer Sheet - all answers should go here 
+// Abgabe Datei - Alle Lösungen müssen hier stehen.
+
+/* 
+except for A-1 and C, you will always write a function that returns values
+Mit Ausnahme von A-1 und C, sollen immer Funktionen mit Rückgabewerten geschrieben werden.
+*/
+
+/* A-1 (5)
+define and fill an object
+Definiere und fülle ein Objekt */
+profileData = null;
+
+/* A-2 (5) */
+convertObjectToArray = null;
+
+/* B-1 (10) */
+convertArrayData = null;
+
+/* B-2 (15) */
+convertArraysToObject = null;
+
+/* B-3 (10) */
+createGrid = null;
+
+/* B-4 (10) */
+wordConverter = null;
+
+/* B-5 (10) */
+calculateHours = null;
+
+/* C (10) 
+write a class
+schreibe eine Klasse */
+Course = null;
+
+/* D-1 (10) */
+capitalizeFirstLetter = null;
+
+/* D-2 (15) */
+validPin = null;
+
+// Do not edit or answer below this line
+// Bearbeite die letzte Zeile nicht. Das wird vom Unit-Test gebraucht
+// EDITING MODULE EXPORTS WILL RESULT IN AN AUTOMATIC FAIL
+module.exports = {profileData, convertObjectToArray, convertArrayData, convertArraysToObject, createGrid, wordConverter, calculateHours, Course, capitalizeFirstLetter, validPin};
